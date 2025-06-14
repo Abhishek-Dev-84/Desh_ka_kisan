f5f92abae2bf856daf98124ce65b676d76cd7aa3
Abhishek Nanda <abhishek.nanda1998@gmail.com>
2025-06-15 00:47:55 +0530

    Update settings.py

--- kisan/settings.py
+++ kisan/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8qvqpc7bx!xn1j)9nm8g2wypp%pvz(p!$$cbj0xcd!ge@603t^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
