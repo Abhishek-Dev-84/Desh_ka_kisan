8bdd21e1f72f2d64c75636e309bd45de664bf325
Abhishek Nanda <abhishek.nanda1998@gmail.com>
2025-06-15 23:44:42 +0530

    Update settings.py

--- kisan/settings.py
+++ kisan/settings.py
@@ -148,7 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME':os.getenv('dcgy56ifq'),
-    'API_KEY': os.getenv('485869871744448'),
-    'API_SECRET': os.getenv('yKV3ywwnsjv1SLSqQvcl90pNYqo'),
+    'CLOUD_NAME':os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
